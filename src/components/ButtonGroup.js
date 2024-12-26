a72bb5597eb6d41aaa7782cf76f194a9a96d8968
daeun <daeun900@naver.com>
2024-12-26 16:26:11 +0900

    241226_btn component 수정정

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -1,5 +1,50 @@
 import React from 'react';
 import DownloadIcon from '@mui/icons-material/Download';
+import styled from 'styled-components';
+
+const BtnWrap = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+`;
+
+const StyledButton = styled.button`
+  background-color: rgba(255, 255, 255, 0.15);
+  box-shadow: 4px 4px 10px 0 rgba(0, 0, 0, 0.25),
+              inset -2px -2px 2px 0px rgba(15, 48, 87, 0.25),
+              inset 2px 2px 2px 0 rgba(255, 255, 255, 0.25);
+  backdrop-filter: blur(20px);
+  border-radius: 20px;
+  height: 100px;
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  align-items: center;
+  justify-items: center;
+  padding: 0 10px;
+  transition: all 0.3s ease;
+  color: #fff;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.6);
+    color: #0057ff;
+  }
+`;
+
+const Txt = styled.div`
+  span {
+    display: block;
+    font-weight: 300;
+  }
+  p {
+    font-size: 20px;
+    line-height: 30px;
+    font-weight: 500;
+  }
+`;
+
+const Icon = styled(DownloadIcon)`
+  font-size: 30px;
+`;
 
 function ButtonGroup({ buttons }) {
   const handleDownload = (link) => {
@@ -7,17 +52,17 @@ function ButtonGroup({ buttons }) {
   };
 
   return (
-    <div className="btn_wrap">
+    <BtnWrap>
       {buttons.map((button, index) => (
-        <button key={index} onClick={() => handleDownload(button.link)}>
-          <div className="txt">
+        <StyledButton key={index} onClick={() => handleDownload(button.link)}>
+          <Txt>
             <span>{button.label}</span>
             <p>{button.description}</p>
-          </div>
-          <DownloadIcon className="icon" />
-        </button>
+          </Txt>
+          <Icon />
+        </StyledButton>
       ))}
-    </div>
+    </BtnWrap>
   );
 }
 
