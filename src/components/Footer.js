45b1fcce3d863ec8b0282ead7c2a6984b7bdd226
daeun <daeun900@naver.com>
2025-06-25 14:19:34 +0900

    250625_리뉴얼_헤더,푸터

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,52 @@
 import React from 'react';
-
+import {Link}  from 'react-router-dom';
+import { CaretRight } from "@phosphor-icons/react";
 function Footer() {
   return (
     <>
      <div className="footer">
-        <div className="wrap">
-          <div className="grid">
-            <div className="img">
-            <span>HRDe Solution 대표: 박병래</span>
-              <img src='/img/hrde-logo-white.png' alt='HRDe Solution'/>
-            </div>
-            <div className="txt">
-              <span>Address</span>
-              <p>경기도 용인시 기흥구 흥덕1로 13 흥덕IT밸리</p>
-              <p>원격평생교육원: 타워동 103호</p>
-              <p>LMS 개발부: 타워동 104호</p>
-              <p>컨텐츠 개발부: 타워동 105호</p>
-              <p>경영지원실: 타워동 103A호</p>
-            </div>
-            <div className="txt">
-              <span>Info</span>
-             
-              <p>사업자등록번호: 591-87-02242</p>
-              <p>Tel: 1811-9530</p>
-              <p>Fax: 031-211-1330 </p>
-              <p>E-Mail: hrde@hrde.co.kr</p>
-            </div>
+        <div className="top">
+          <div className="wrap">
+            <ul className="link">
+              <li><Link to="/">회사 소개</Link></li>
+              <li><Link to="/">찾아오시는 길</Link></li>
+            </ul>
+            <ul className="social">
+              <li><Link to="http://pf.kakao.com/_xaSSxgxj/chat"><img src="/img/footer-soc1.png" alt="카카오톡 바로가기" /></Link></li>
+              <li><Link to="https://www.youtube.com/@hrd9130"><img src="/img/footer-soc2.png" alt="블로그 바로가기" /></Link></li>
+              <li><Link to="https://blog.naver.com/hrdesolution"><img src="/img/footer-soc3.png" alt="유튜브 바로가기" /></Link></li>
+            </ul>
           </div>
-          <div className="copyright">
-            COPYRIGHT ⓒ HRDe Solution Inc. ALL RIGHTS RESERVED
+          </div>
+          <div className="bottom">
+            <div className="wrap">
+              <div className="left">
+                <ul>
+                  <li><b>(주)HRDe 솔루션</b></li>
+                  <li>대표자: 박병래</li>
+                  <li>사업자등록번호 : 591-87-02242</li>
+                  <li>개인정보관리자 : 노현정</li>
+                </ul>
+                <ul>
+                  <li>주소 : 경기도 용인시 기흥구 흥덕1로 13 흥덕 IT밸리 타워동 105호</li>
+                  <li>E-Mail : hrde@hrde.co.kr</li>
+                </ul>
+                <ul>
+                  <li>COPYRIGHT ⓒ HRDe Solution Inc. ALL RIGHTS RESERVED.</li>
+                </ul>
+              </div>
+              <div className="right">
+                <div className="icn"><img src="/img/footer-icn.png" alt="아이콘"/>고객센터<CaretRight size={16}/></div>
+                <div className="cs">
+                  <strong>1811-9530</strong>
+                  <p><b>상담시간</b>평일 09:00 ~ 18:00</p>
+                  <p><b>휴뮤일</b>토요일, 일요일, 공휴일</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-     </div>
+
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
-  const location = useLocation(); // 현재 경로를 가져옴
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) { // 스크롤이 50px 이상 내려가면
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+      setIsScrolled(window.scrollY > 50);
     };
 
-     // 특정 경로에서만 스크롤 이벤트 활성화
     if (location.pathname === '/') {
       window.addEventListener('scroll', handleScroll);
-    } else {
-      setIsScrolled(false); // 다른 페이지에서는 초기 상태 유지
     }
 
     return () => {
-      window.removeEventListener('scroll', handleScroll); // 이벤트 리스너 제거
+      window.removeEventListener('scroll', handleScroll);
     };
-  }, [location.pathname]); // 경로가 변경될 때마다 effect 재실행
+  }, [location.pathname]);
+
+  const headerClass =
+    location.pathname === '/' ? (isScrolled ? '' : 'unscrolled') : '';
+
+  // 로고 이미지 선택
+  const logoSrc =
+    location.pathname === '/' && !isScrolled
+      ? '/img/header-logo-white.png'
+      : '/img/header-logo.png';
 
   return (
-    <>
-    <header className={location.pathname === '/' ? (isScrolled ? '' : 'unscrolled') : ''}>
-        <div className='wrap'>
-            <nav className='headerNav'>
-                <Link to="/"><img src='/img/header-logo.png' alt='HRDe Solution'/></Link>
-                <ul>
-                    <li><Link to="/about">회사소개</Link></li>
-                    <li><Link to="/lms">HRDe LMS</Link></li>
-                    <li><Link to="/contents">Contents</Link></li>
-                    <li><Link to="/consulting">Consulting</Link></li>
-                    <li><Link to="/contact">모아</Link></li>
-                </ul>
-            </nav>
-        </div>
+    <header className={headerClass}>
+      <div className="wrap">
+        <nav className="headerNav">
+          <Link to="/">
+            <img src={logoSrc} alt="HRDe Solution" />
+          </Link>
+          <ul>
+            <li><Link to="/lms">LMS</Link></li>
+            <li><Link to="/contents">CONTENTS</Link></li>
+            <li><Link to="/consulting">CONSULTING</Link></li>
+            <li><Link to="/contact">모아</Link></li>
+            <li><Link to="/" className='btn'>상담문의</Link></li>
+          </ul>
+        </nav>
+      </div>
     </header>
-    </>
-
- 
   );
 }
 
