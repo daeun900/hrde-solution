4158e5a562d367ba686e3aad2948f487ae501f9d
daeun <daeun900@naver.com>
2025-07-09 17:14:52 +0900

    250709

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './css/common.scss'
 import Header from './components/Header'; 
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import About from './pages/About';
 import LMS from './pages/LMS';
 import Contents from './pages/Contents';
 import Consulting from './pages/Consulting';
@@ -17,7 +16,6 @@ function App() {
     <Header />  
     <Routes>
       <Route path="/" element={<Home />} />   
-      <Route path="/about" element={<About/>} />
       <Route path="/lms" element={<LMS/>} />  
       <Route path="/contents" element={<Contents/>} />  
       <Route path="/consulting" element={<Consulting/>} />  
