16fd9b952efb39a24ffeb43923314b461e024a26
daeun <daeun900@naver.com>
2025-07-11 15:53:06 +0900

    250711

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './css/index.css' 
 import './css/common.scss'
 import Header from './components/Header'; 
@@ -12,10 +12,21 @@ import Consult from './pages/Consult';
 import News from './pages/News';
 import NewsDetail from './pages/NewsDetail';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
   <Router>
     <Header />  
+    <ScrollToTop /> 
     <Routes>
       <Route path="/" element={<Home />} />   
       <Route path="/lms" element={<LMS/>} />  
